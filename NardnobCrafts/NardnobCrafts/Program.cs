48442d06e0f0be8592d1d77aff18334c1fa46fbb
Brandon <nardnob@users.noreply.github.com>
2023-09-06 19:24:16 -0500

    Add JsonFileProductsService

--- NardnobCrafts/NardnobCrafts/Program.cs
+++ NardnobCrafts/NardnobCrafts/Program.cs
@@ -1,7 +1,10 @@
+using NardnobCrafts.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<JsonFileProductsService>();
 
 var app = builder.Build();
 
---
+++ NardnobCrafts/NardnobCrafts/Services/JsonFileProductsService.cs
@@ -0,0 +1,32 @@
+﻿using NardnobCrafts.Models;
+using System.Text.Json;
+
+namespace NardnobCrafts.Services
+{
+    public class JsonFileProductsService
+    {
+        public JsonFileProductsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
